633f46e7b5cde52fc5f664318a1be3fa648e6c89
expee <experian.elitiawan@gmail.com>
2018-07-22 01:15:40 +0700

    add git ignore and fix build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+if [ ! -d "_build" ]; then
+	mkdir _build
+fi
+
 gfortran -c -fdec-structure -o ./_build/MeshParserFortran.o MeshParser.f90 
 gcc -c -o ./_build/MeshParserC.o MeshParser.c 
 gfortran -o ./_build/MeshParser ./_build/MeshParserC.o ./_build/MeshParserFortran.o
