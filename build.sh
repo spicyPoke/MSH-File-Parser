e9644e0986fa903ebea797caae285fc0368d240f
expee <experian.elitiawan@gmail.com>
2018-08-01 00:08:46 +0700

    remove -fdec-structure command line argument

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ if [ ! -d "_build" ]; then
 	mkdir _build
 fi
 
-gfortran -c -fdec-structure -o ./_build/MeshParserFortran.o MeshParser.f90 
+gfortran -c -o ./_build/MeshParserFortran.o MeshParser.f90 
 gcc -c -o ./_build/MeshParserC.o MeshParser.c 
 gfortran -o ./_build/MeshParser ./_build/MeshParserC.o ./_build/MeshParserFortran.o
